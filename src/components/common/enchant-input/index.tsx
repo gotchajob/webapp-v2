915e020648dde9c31cccb1e25067ff4a8842db51
Kiet <kietly019@gmail.com>
2024-05-14 21:02:00 +0700

    update

---
+++ src/components/common/enchant-input/index.tsx
@@ -0,0 +1,42 @@
+import { Editor } from '@tinymce/tinymce-react';
+
+export const EnchantInput = ({ initValue, onChange }: { initValue: string; onChange?: (value: string) => void }) => {
+  return (
+    <Editor
+      inline={true}
+      //@ts-ignore
+      onChange={onChange}
+      apiKey="e2f3pntb5ogxx9hu1lba5p8ef4c29vnogx4n8lid5dw71i4v"
+      initialValue={initValue}
+      init={{
+        width: 500,
+        menubar: false,
+        plugins: [
+          'advlist',
+          'autolink',
+          'lists',
+          'link',
+          'image',
+          'charmap',
+          'preview',
+          'anchor',
+          'searchreplace',
+          'visualblocks',
+          'code',
+          'fullscreen',
+          'insertdatetime',
+          'media',
+          'table',
+          'code',
+          'help',
+          'wordcount'
+        ],
+        toolbar:
+          'undo redo  | ' +
+          'bold italic forecolor | alignleft aligncenter ' +
+          'alignright alignjustify | bullist numlist outdent indent | ',
+        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+      }}
+    />
+  );
+};
