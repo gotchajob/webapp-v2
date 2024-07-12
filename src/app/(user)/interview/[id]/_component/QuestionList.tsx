68fc3f960d0e7b70c9abb53f2f73592b42baf274
Phu <phulk19012002@gmail.com>
2024-07-12 15:32:32 +0700

    update branch

---
+++ src/app/(user)/interview/[id]/_component/QuestionList.tsx
@@ -0,0 +1,139 @@
+"use client";
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+
+// project imports
+import SubCard from 'ui-component/cards/SubCard';
+import Chip from 'ui-component/extended/Chip';
+import { gridSpacing } from 'store/constant';
+import { format } from 'date-fns';
+
+// assets
+import CalendarTodayTwoToneIcon from '@mui/icons-material/CalendarTodayTwoTone';
+import DeleteTwoToneIcon from '@mui/icons-material/DeleteTwoTone';
+import EmailTwoToneIcon from '@mui/icons-material/EmailTwoTone';
+import PhoneAndroidTwoToneIcon from '@mui/icons-material/PhoneAndroidTwoTone';
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+
+// types
+import { ThemeMode } from 'types/config';
+import { useEffect, useState } from 'react';
+import { Button, Dialog, DialogContent, Rating, TextField } from '@mui/material';
+import { StyledLink } from 'components/common/link/styled-link';
+import Image from 'next/image';
+
+const formatDate = (isoString: any) => {
+    return format(new Date(isoString), 'HH:mm dd/MM/yyyy');
+};
+
+const QuestionListPage = () => {
+
+    return (
+        <SubCard>
+            <Grid container spacing={gridSpacing} px={5}>
+                <Grid item xs={12}>
+                    <Grid container spacing={gridSpacing}>
+                        <Grid item xs={12}>
+                            <Grid container spacing={1}>
+                                <Grid item>
+                                    <Typography variant="body2">
+                                        Danh sách câu hỏi buổi phỏng vấn :
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Divider />
+                        </Grid>
+                        
+                        <Grid item xs={12}>
+                            <Grid container spacing={gridSpacing}>
+                                <Grid item xs={12} sm={6} md={4}>
+                                    <Stack spacing={2}>
+                                        <Typography variant="h4">Thông tin khách hàng</Typography>
+                                        <Stack spacing={0}>
+                                            <Stack direction="row" spacing={1}>
+                                                <Typography variant="subtitle1">Tên khách hàng :</Typography>
+                                                <Typography variant="body2">Anshan Handgun</Typography>
+                                            </Stack>
+                                            <Stack direction="row" spacing={1}>
+                                                <Typography variant="subtitle1">Email :</Typography>
+                                                <Typography variant="body2">anshan.handgun@example.com</Typography>
+                                            </Stack>
+                                        </Stack>
+                                    </Stack>
+                                </Grid>
+                                <Grid item xs={12} sm={6} md={4}>
+                                    <Stack spacing={2}>
+                                        <Typography variant="h4">Buổi phỏng vấn</Typography>
+                                        <Stack spacing={0}>
+                                            <Stack direction="row" spacing={1}>
+                                                <Typography variant="subtitle1">Kỹ năng khách hàng chọn phỏng vấn :</Typography>
+                                                <Typography variant="body2">ReactJS, NodeJS</Typography>
+                                            </Stack>
+                                        </Stack>
+                                    </Stack>
+                                </Grid>
+                                <Grid item xs={12} sm={6} md={4}>
+                                    <Stack spacing={0} sx={{ mt: { xs: 0, md: 3 } }}>
+                                        <Stack direction="row" spacing={1}>
+                                            <Typography variant="subtitle1">Trạng thái :</Typography>
+                                            <Chip label="Đã đặt lịch" variant="outlined" size="small" chipcolor='warning'
+                                            />
+                                        </Stack>
+                                    </Stack>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Divider />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Typography variant="h4" sx={{ mt: 1 }}>Câu hỏi phỏng vấn</Typography>
+                            <Grid container spacing={2} sx={{ mt: 1 }}>
+
+                                <Grid item xs={12} md={6}>
+                                    <Stack spacing={1}>
+                                        <Typography variant="subtitle1">1. Vui lòng mô tả kinh nghiệm làm việc với ReactJS:</Typography>
+                                        <TextField fullWidth variant="outlined" multiline rows={2} placeholder="Nhập câu trả lời của bạn" />
+                                    </Stack>
+                                </Grid>
+
+                                <Grid item xs={12} md={6}>
+                                    <Stack spacing={1}>
+                                        <Typography variant="subtitle1">2. Bạn đã làm việc với NodeJS bao nhiêu năm?</Typography>
+                                        <TextField fullWidth variant="outlined" type="number" placeholder="Nhập số năm kinh nghiệm" />
+                                    </Stack>
+                                </Grid>
+
+                                <Grid item xs={12} md={6}>
+                                    <Stack spacing={1}>
+                                        <Typography variant="subtitle1">3. Đánh giá kỹ năng ReactJS của bạn từ 1 đến 5:</Typography>
+                                        <Rating name="rating" defaultValue={0} size="large" />
+                                    </Stack>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+
+                    </Grid>
+                </Grid>
+            </Grid>
+        </SubCard>
+    );
+};
+
+export default QuestionListPage;
---
+++ src/app/(user)/interview/[id]/page.tsx
@@ -0,0 +1,169 @@
+'use client';
+
+import Link from 'next/link';
+import { SyntheticEvent, useState } from 'react';
+
+// material-ui
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
+import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material/styles';
+
+// project imports
+import { gridSpacing } from 'store/constant';
+import MainCard from 'ui-component/cards/MainCard';
+
+// assets
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccountCircleTwoToneIcon from '@mui/icons-material/AccountCircleTwoTone';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import ReceiptIcon from '@mui/icons-material/Receipt';
+
+// types
+import { TabsProps } from 'types';
+import { ThemeMode } from 'types/config';
+import CustomerProfilePage from './_component/ExpertProfile';
+import QuestionListPage from './_component/QuestionList';
+
+
+// tabs panel
+function TabPanel({ children, value, index, ...other }: TabsProps) {
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && <Box>{children}</Box>}
+        </div>
+    );
+}
+
+function a11yProps(index: number) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+// tabs option
+const tabsOption = [
+    {
+        label: 'Thông tin khách hàng',
+        icon: <AccountCircleIcon sx={{ fontSize: '1.3rem' }} />,
+        caption: 'Thông tin hồ sơ khách hàng'
+    },
+    {
+        label: 'Danh sách câu hỏi',
+        icon: <ReceiptIcon sx={{ fontSize: '1.3rem' }} />,
+        caption: 'Danh sách câu hỏi'
+    },
+];
+
+// ==============================|| EXPERT BOOKING PAGE ||============================== //
+
+const InterViewPage = ({ params }: { params: { id: string } }) => {
+    const theme = useTheme();
+
+    const [value, setValue] = useState<number>(0);
+
+    const handleChange = (event: SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+
+    return (
+        <Box sx={{ boxShadow: 3, my: 5, mx: 10 }}>
+            <MainCard>
+                <Grid container spacing={gridSpacing} >
+                    <Grid item xs={12}>
+                        <Tabs
+                            value={value}
+                            onChange={handleChange}
+                            aria-label="icon label tabs example"
+                            variant="scrollable"
+                            sx={{
+                                '& .MuiTabs-flexContainer': {
+                                    borderBottom: 'none'
+                                },
+                                '& .MuiTabs-indicator': {
+                                    display: 'none'
+                                },
+                                '& .MuiButtonBase-root + .MuiButtonBase-root': {
+                                    position: 'relative',
+                                    overflow: 'visible',
+                                    ml: 2,
+                                    '&:after': {
+                                        content: '""',
+                                        bgcolor: '#ccc',
+                                        width: 1,
+                                        height: 'calc(100% - 16px)',
+                                        position: 'absolute',
+                                        top: 8,
+                                        left: -8
+                                    }
+                                }
+                            }}
+                        >
+                            {tabsOption.map((tab, index) => (
+                                <Tab
+                                    value={index}
+                                    key={index}
+                                    icon={tab.icon}
+                                    label={
+                                        <Grid container direction="column">
+                                            <Typography variant="subtitle1" color="inherit">
+                                                {tab.label}
+                                            </Typography>
+                                            <Typography variant="caption" sx={{ textTransform: 'capitalize' }}>
+                                                {tab.caption}
+                                            </Typography>
+                                        </Grid>
+                                    }
+                                    sx={{
+                                        minHeight: 'auto',
+                                        minWidth: { xs: '100%', md: 250 },
+                                        padding: 2,
+                                        borderRadius: `${theme.shape.borderRadius}px`,
+                                        display: 'flex',
+                                        flexDirection: 'row',
+                                        alignItems: 'flex-start',
+                                        textAlign: 'left',
+                                        justifyContent: 'flex-start',
+                                        '&:after': {
+                                            bgcolor: 'transparent !important'
+                                        },
+                                        '&.Mui-selected': {
+                                            color: 'primary.main',
+                                            bgcolor: theme.palette.mode === ThemeMode.DARK ? 'dark.main' : 'grey.50'
+                                        },
+                                        '& > svg': {
+                                            marginBottom: '0px !important',
+                                            mr: 1.25,
+                                            mt: 0.25,
+                                            height: 20,
+                                            width: 20
+                                        }
+                                    }}
+                                />
+                            ))}
+                        </Tabs>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TabPanel value={value} index={0}>
+                            <CustomerProfilePage />
+                        </TabPanel>
+                        <TabPanel  value={value} index={1}>
+                            <QuestionListPage />
+                        </TabPanel>
+                    </Grid>
+                </Grid>
+            </MainCard>
+        </Box>
+    );
+};
+
+export default InterViewPage;
