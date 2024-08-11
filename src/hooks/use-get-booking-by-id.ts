7c0cd9b092face76cb46070b323b1606b098386d
Kiet <kietly019@gmail.com>
2024-08-11 20:29:03 +0700

    update

--- src/hooks/use-get-booking-by-id.ts
+++ src/hooks/use-get-booking-by-id.ts
@@ -4,21 +4,23 @@ import { GetCVTemplate } from './../package/api/cv-template/index';
 import { BookingById, GetBookingById, GetBookingByIdRequest } from 'package/api/booking/id';
 
 export const UseGetBookingById = (params: GetBookingByIdRequest, refresh: number) => {
-    const [bookingById, setBookingById] = useState<BookingById | undefined>(undefined);
+  const [bookingById, setBookingById] = useState<BookingById | undefined>(undefined);
 
-    const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    const fetchBookingById = async () => {
-        try {
-            setLoading(true);
-            const data = await GetBookingById(params);
-            setBookingById(data.data);
-        } catch (error: any) {
-        } finally {
-            setLoading(false);
-        }
+  const fetchBookingById = async () => {
+    try {
+      setLoading(true);
+      const data = await GetBookingById(params);
+      setBookingById(data.data);
+    } catch (error: any) {
+    } finally {
+      setLoading(false);
     }
+  };
 
-    useEffect(() => { fetchBookingById(); }, [params.id, refresh]);
-    return ({ bookingById, loading })
-}
+  useEffect(() => {
+    fetchBookingById();
+  }, [params.id, refresh]);
+  return { bookingById, loading };
+};
