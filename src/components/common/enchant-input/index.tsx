dc79c93b30c71fe5bec47cd433ff9cde6006e3ba
Kiet <kiet019@gmail.com>
2024-09-29 11:53:08 +0700

    update

--- src/components/common/enchant-input/index.tsx
+++ src/components/common/enchant-input/index.tsx
@@ -1,53 +1,64 @@
-import { Editor } from '@tinymce/tinymce-react';
+import { Editor } from "@tinymce/tinymce-react";
+import { useState } from "react";
 
-export const EnchantInput = ({ initValue, onChange, onBlur }: { initValue: string; onChange?: (value: string) => void; onBlur?: (value: string) => void }) => {
+export const EnchantInput = ({
+  initValue,
+  onBlur,
+  onChange,
+}: {
+  onBlur?: any;
+  initValue: string;
+  onChange?: (value: string) => void;
+}) => {
+  const [data, setData] = useState(initValue);
 
-  const handleEditorChange = (content: string) => {
+  const handleBlur = () => {
     if (onChange) {
-      onChange(content);
-    }
-  };
-
-  const handleEditorBlur = (e: any, editor: any) => {
-    if (onBlur) {
-      onBlur(editor.getContent());
+      onChange(data);
     }
   };
 
   return (
     <Editor
       inline={true}
-      onEditorChange={handleEditorChange}
-      onBlur={handleEditorBlur}
+      //@ts-ignore
+      onEditorChange={(value: string) => {
+        setData(value);
+      }}
+      onBlur={(e) => {
+        console.log(e.target.value);
+        handleBlur();
+      }}
       apiKey="gvvfxt64x0hg3cpxxu16ftwrn23hiz8u4hm9s6rto4d65mdl"
       initialValue={initValue}
       init={{
         width: 500,
         menubar: false,
         plugins: [
-          'advlist',
-          'autolink',
-          'lists',
-          'link',
-          'image',
-          'charmap',
-          'preview',
-          'anchor',
-          'searchreplace',
-          'visualblocks',
-          'code',
-          'fullscreen',
-          'insertdatetime',
-          'media',
-          'table',
-          'code',
-          'help',
-          'wordcount'
+          "advlist",
+          "autolink",
+          "lists",
+          "link",
+          "image",
+          "charmap",
+          "preview",
+          "anchor",
+          "searchreplace",
+          "visualblocks",
+          "code",
+          "fullscreen",
+          "insertdatetime",
+          "media",
+          "table",
+          "code",
+          "help",
+          "wordcount",
         ],
         toolbar:
-          'fontsize |' +
-          'bold italic forecolor |' +
-          'bullist numlist lineheight',
+          "bold italic forecolor | fontsize | lineheight | alignleft aligncenter " +
+          "alignright alignjustify | bullist numlist outdent indent | ",
+        content_style:
+          "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
       }}
     />
   );
